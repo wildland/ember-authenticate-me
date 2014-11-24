542d04456765c93183fce208a2f8ed159a8e85a3
Sam Clopton <samsinite@gmail.com>
2014-11-24 11:00:15 -0800

    # This is a combination of 2 commits. # The first commit's message is:
    bumped version for release
    
    # This is the 2nd commit message:
    
    WIP...

---
+++ addon/torii-adapters/base.js
@@ -0,0 +1,97 @@
+import Ember from 'ember';
+
+export default Ember.Object.extend({
+  sessionKey: null,
+
+  sessionUri: '/api/session',
+
+  _ajaxOptions: function(authorizaton) {
+    return {
+      url: this.get('sessionUri'),
+      data: JSON.stringify(authorizaton),
+      type: 'POST',
+      dataType: 'json',
+      contentType: 'application/json',
+    };
+  },
+
+  _ajaxError: function(jqXHR/*, responseText*/) {
+    if (jqXHR && typeof jqXHR === 'object') {
+      jqXHR.then = null;
+    }
+
+    return jqXHR;
+  },
+
+  open: function(authorizaton) {
+    var self = this,
+    hash = this._ajaxOptions(authorizaton);
+
+    return new Ember.RSVP.Promise(function(resolve, reject) {
+      hash.success = function(json/*, textStatus, jqXHR*/) {
+        Ember.run(null, resolve, json);
+      };
+
+      hash.error = function(jqXHR/*, textStatus, errorThrown*/) {
+        Ember.run(null, reject, this._ajaxError(jqXHR, jqXHR.responseText));
+      };
+
+      Ember.$.ajax(hash);
+    }).then(function(params) {
+      var session = self.container.lookup('torii:session');
+
+      session.set('content', { token: params.session.key });
+      self.set('token', params.session.key);
+
+      return {
+        token: params.session.key,
+        expiration: params.session.expiration
+      };
+    });
+  },
+
+  /**
+  * This method destroys/unauthenticates the existing session with the server.
+  **/
+  close: function() {
+    //TODO: Lookup token via local storage, and query the server using the token.
+    var token = this.get('token');
+
+    return new Ember.RSVP.Promise(function(resolve, reject) {
+      Ember.$.ajax({
+        url: this.get('sessionUri'),
+        type: 'DELETE',
+        dataType: 'json',
+        contentType: 'application/json',
+        headers: { Authorization: 'Token token=' + token },
+        success: Ember.run.bind(null, resolve),
+        error: Ember.run.bind(null, reject)
+      }).then(function(data) {
+        return data;
+      });
+    });
+  },
+
+  /**
+  * This method is for checking to see if there is a session saved somewhere in memory
+  * and then verifying that it is valid.
+  **/
+  fetch: function() {
+    //TODO: Lookup token via local storage, query the server using the token, and return the session
+    var token = this.get('token');
+
+    return new Ember.RSVP.Promise(function(resolve, reject) {
+      Ember.$.ajax({
+        url: this.get('sessionUri'),
+        type: 'GET',
+        dataType: 'json',
+        contentType: 'application/json',
+        headers: { Authorization: 'Token token=' + token },
+        success: Ember.run.bind(null, resolve),
+        error: Ember.run.bind(null, reject)
+      }).then(function(data) {
+        return data;
+      });
+    });
+  }
+});
