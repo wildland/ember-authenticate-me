5e38267d14d6f5c4266012b6485e7cbd986bee55
Joe Weakley <joew@samjoe.com>
2015-01-28 11:27:19 -0800

    Added support for currentPassword.

--- app/serializers/user.js
+++ app/serializers/user.js
@@ -10,15 +10,18 @@ export default DS.ActiveModelSerializer.extend({
   },
 
   serialize: function(record/*, options*/) {
-    var json = this._super.apply(this, arguments),
-        password = record.get('password'),
-        passwordConfirmation = record.get('passwordConfirmation');
+    var json = this._super.apply(this, arguments);
+    var password = record.get('password');
+    var passwordConfirmation = record.get('passwordConfirmation');
+    var currentPassword = record.get('currentPassword');
 
     json.password = password;
     json.password_confirmation = passwordConfirmation;
+    json.current_password = currentPassword;
 
     record.set('password', null);
     record.set('passwordConfirmation', null);
+    record.set('currentPassword', null);
 
     return json;
   }
