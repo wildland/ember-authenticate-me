5f6080aa7db46c79b3a965d36a864da029462dc3
Sam Clopton <samsinite@gmail.com>
2015-04-13 13:42:39 -0700

    upgraded ember-cli and torii to latest

--- blueprints/ember-authenticate-me/index.js
+++
@@ -1,9 +0,0 @@
-/* global module */
-
-module.exports = {
-  afterInstall: function(options) {
-    return this.addPackageToProject("torii", "^0.2");
-  },
-
-  normalizeEntityName: function() {}
-};
