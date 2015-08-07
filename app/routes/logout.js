0be3391256bcda9b3e64a68e89a80fb5e52dfdb1
Sam Clopton <samsinite@gmail.com>
2015-08-06 17:25:55 -0700

    Allow customizable login transition. Fixes #22
    bug fix
    
    hound report fixes
    
    fix app/controller/login export and make hound shutup about es6 modules
    
    removed duplicate esnext jshint config

--- app/routes/logout.js
+++ app/routes/logout.js
@@ -1,8 +1,11 @@
 import Ember from 'ember';
+import AuthenticatedMixin from 'ember-authenticate-me/mixins/routes/authenticated';
 
-export default Ember.Route.extend({
+export default Ember.Route.extend(AuthenticatedMixin, {
   beforeModel: function() {
     var self = this;
+
+    this._super(...arguments);
 
     this.get('session').close().then(function() {
       self.transitionTo('login');
