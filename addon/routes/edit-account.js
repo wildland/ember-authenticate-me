c27a657f01d4f7214d12fc7bf57e3d097b616f2d
Sam Clopton <samsinite@gmail.com>
2017-03-15 14:19:19 -0700

    fix a few store/namespace bugs

--- addon/routes/edit-account.js
+++ addon/routes/edit-account.js
@@ -2,6 +2,6 @@ import AuthenticatedRoute from 'ember-authenticate-me/routes/authenticated';
 
 export default AuthenticatedRoute.extend({
   model: function(params) {
-    return this.get('store').find('user', params.user_id);
+    return this.get('store').find('token-authenticate-me/user', params.user_id);
   }
 });
--- addon/routes/sign-up.js
+++ addon/routes/sign-up.js
@@ -2,7 +2,7 @@ import Ember from 'ember';
 
 export default Ember.Route.extend({
   model: function() {
-    var user = this.get('store').createRecord('user');
+    var user = this.get('store').createRecord('token-authenticate-me/user');
 
     return user;
   }
